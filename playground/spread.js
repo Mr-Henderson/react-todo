bca14851bfbb2ba56c2b304de418b5b297c5f70e
Your Name <bigtob@gmail.com>
2017-08-29 05:45:27 -0500

    Added playground folder testing spread operator

---
+++ playground/spread.js
@@ -0,0 +1,33 @@
+// var add = Blurb;
+//
+// function Blurb (a, b) {
+//   return a + b;
+// }
+//
+// console.log(Blurb(3, 1));
+//
+// var toBlurb = [9, 5];
+// console.log(Blurb(...toBlurb));
+
+// var groupA = ['Ryan', 'John'];
+// var groupB = ['Sammy'];
+// var final = [...groupA, 3, ...groupB];
+//
+// console.log(final);
+
+var Person = ['Jim', 21];
+var PersonTwo = ['Emmit', 99];
+//Hi Jim you are 25
+
+function Greet (name, age) {
+  console.log('Hi ' + name + ', you are ' + age);
+};
+Greet(...Person);
+Greet(...PersonTwo);
+
+var names = ['Mike', 'Ben'];
+var final = ['Toby', ...names];
+// console.log(final(...names, final));
+final.forEach(function (Blurb) {
+  console.log('Hi ' + Blurb);
+});
